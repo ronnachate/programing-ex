4a55b167a77e832a424853e37d7bd7f81a379b47
Ronnachate <ronnachate@gmail.com>
2016-12-22 00:42:01 +0700

    Add comment.

--- src/Programing/Ex/Calculator.cs
+++ src/Programing/Ex/Calculator.cs
@@ -28,9 +28,11 @@ namespace Programing.Ex
         {
             double profit = 0.0;  
             if( transactions.Count > 0 ) {
+                //get sale transaction
                 var saleTransactions = transactions.Where( t => t.Type == SALE_TYPE ).ToList();
                 foreach (var transaction in saleTransactions )
-                {                    
+                {
+                    //get buy transaction for this ProductId
                     var buyTransactions = transactions.Where( t => t.Type == BUY_TYPE )
                         .Where( t => t.ProductId == transaction.ProductId )
                         .ToList();
@@ -38,12 +40,16 @@ namespace Programing.Ex
                     foreach (var buyTransaction in buyTransactions )
                     {
                         if( quantityLeft > 0 ) {
+                            // sale quantity overflow from last buy trasaction
                             if( quantityLeft <= buyTransaction.Quantity ) {
+                                // buy transaction enought for overflow quantity 
                                 profit += quantityLeft * ( transaction.Price - buyTransaction.Price );
+                                //update buyTransaction.Quantity
                                 buyTransaction.Quantity = buyTransaction.Quantity - quantityLeft;
                                 break;
                             }
                             else {
+                                //overflow to next buy trasaction
                                 profit += buyTransaction.Quantity * ( transaction.Price - buyTransaction.Price );
                                 quantityLeft = quantityLeft - buyTransaction.Quantity;
                                 transactions.Remove(buyTransaction);                      
@@ -52,11 +58,14 @@ namespace Programing.Ex
                         else {
                             if( transaction.Quantity <= buyTransaction.Quantity )
                             {
+                                // buy transaction enought for sale transaction
                                 profit +=  transaction.Quantity * ( transaction.Price - buyTransaction.Price );
+                                //update buyTransaction.Quantity
                                 buyTransaction.Quantity = buyTransaction.Quantity - transaction.Quantity;
                                 break;
                             }
                             else {
+                                //overflow to next buy trasaction
                                 profit += buyTransaction.Quantity * ( transaction.Price - buyTransaction.Price );
                                 quantityLeft = transaction.Quantity - buyTransaction.Quantity;
                                 transactions.Remove(buyTransaction);
